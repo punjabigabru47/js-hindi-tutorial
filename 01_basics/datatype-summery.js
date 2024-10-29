ea1c369af2315e48b59a9420655d78d46d4d7f7f
punjabigabru47 <153139516+punjabigabru47@users.noreply.github.com>
2024-10-29 22:50:16 +0000

    stack-and-heap-explaination

--- 01_basics/datatype-summery.js
+++ 01_basics/datatype-summery.js
@@ -5,3 +5,11 @@
 // non-primitive or reference
    
            // array, functions and objects
+// -------------------------------------------------------------------------------------
+
+// memory 
+//stack (primitive)      heap(non primitve)
+
+// cant change the original value of variable but can do in copy of that variable.
+// but in heap if we reassigne variable or object and do any change in latest object 
+// it will change the value in orignal object.
