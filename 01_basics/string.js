82acb7317e0be6f57e206cbaf4f30b582fe4acc9
punjabigabru47 <153139516+punjabigabru47@users.noreply.github.com>
2024-10-29 23:26:50 +0000

    string-methods

---
+++ 01_basics/string.js
@@ -0,0 +1,12 @@
+const gameName = new String("sunnysingh")
+
+// console.log(gameName);
+// console.log(gameName.toLocaleUpperCase())
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf('y'))
+
+// in slice method we can give negative value
+console.log(gameName.slice(5, 5))
+
+//console.log(gameName.slice(-7, 5))
+
