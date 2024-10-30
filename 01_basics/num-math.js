cbf0f2f207dd672849f57df0e31ac7bd82f30015
punjabigabru47 <153139516+punjabigabru47@users.noreply.github.com>
2024-10-30 18:30:06 +0000

    math and number

---
+++ 01_basics/num-math.js
@@ -0,0 +1,7 @@
+const balance = new Number(1000)
+
+console.log(balance.toString().length)
+console.log(balance.toFixed(2))
+
+const othernum =  1000000
+console.log(othernum.toLocaleString('en-IN'))
