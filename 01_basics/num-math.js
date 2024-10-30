bd556a6ef99d9f63458c52e9a4647607378ba633
punjabigabru47 <153139516+punjabigabru47@users.noreply.github.com>
2024-10-30 18:55:46 +0000

    math random and floor explain

--- 01_basics/num-math.js
+++ 01_basics/num-math.js
@@ -5,3 +5,14 @@ console.log(balance.toFixed(2))
 
 const othernum =  1000000
 console.log(othernum.toLocaleString('en-IN'))
+
+//--------------------------math-----------------------
+
+console.log(Math.random()) // recieve random number
+
+console.log((Math.random() * 10) + 1) // +1 is added to eleminate 0 figure.
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min)) + min)
