835fd138b6ddcf644e930abf51bd6c004689cace
punjabigabru47 <153139516+punjabigabru47@users.noreply.github.com>
2024-10-31 18:27:55 +0000

    array-methods

---
+++ 01_basics/array.js
@@ -0,0 +1,21 @@
+const myarr = new Array(1,2,3,4,5)
+console.log(myarr[1])
+
+// methods
+
+myarr.push(6) // will push 6 into array at last 
+myarr.pop()  // will remove last element of the an array
+myarr.unshift(7) // it will add element at the begening of the array.
+myarr.shift() // will remove the first element of an array.
+
+
+
+
+// slice and splice
+
+const mna1 = myarr.slice(1,3) // returns a section of array but dont manuplate original array
+const mna2 = myarr.splice(1,3) // it manuplate an array, it includes 3 as well
+console.log(myarr)
+console.log(mna1)
+console.log(mna2)
+console.log(myarr)
