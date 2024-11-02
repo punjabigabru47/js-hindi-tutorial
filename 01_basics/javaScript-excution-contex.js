82d139e8ab6605e837acd3ef3ca029b0a2313b9f
punjabigabru47 <153139516+punjabigabru47@users.noreply.github.com>
2024-11-02 23:05:42 +0000

    js execution context

---
+++ 01_basics/javaScript-excution-contex.js
@@ -0,0 +1,32 @@
+/*
+javaScript execution Context
+
+step one: Global execution context has been created and put in "this" variable in browser.
+
+-> global execution context
+-> functional execution context
+-> eval execution context
+
+-> two phases of execution
+  1. memory creation phase
+  2. execution phase
+
+1. when we run any code in js fist this happens is "global execution and allocate
+    in "this" variable in browser enviornment.
+2. memory creation phase-
+    we declare all the variables and their first value by default is "undefined"
+    and any function stored as defined or definition.
+    even the results are alse undefined in memory creation phase.
+3. execution phase-
+   1. first thing happens assigns the values to variables we created which were 
+      undefined bydefault and in execution phase we assigne them a value.
+   
+
+
+
+
+
+
+
+
+*/
