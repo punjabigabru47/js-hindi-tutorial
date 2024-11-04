3d1b381feebfccdaa5a18f89e8db6afe9540774c
punjabigabru47 <153139516+punjabigabru47@users.noreply.github.com>
2024-11-04 22:34:13 +0000

    advance methds

---
+++ 01_basics/one.js
@@ -0,0 +1,35 @@
+const month = "mar"
+
+ //switch (month) {
+// //     case "jan":
+// //         console.log("january")
+// //         break;
+// //     case "feb":
+// //         console.log("february")
+// //         break;
+// //     case "mar":
+// //         console.log("march")
+// //         break;
+// //     case "apr":
+// //         console.log("april")
+// //         break;
+
+
+// }
+        //if u want to check if array is empty
+
+        const array = []
+
+        if(array.length === 0){
+            console.log("array is empty")
+        }
+
+        // if u want to check if object is empty
+
+        const obj = {}
+
+        if(Object.keys(obj).length === 0){
+            console.log("object is empty")
+        }
+
+
